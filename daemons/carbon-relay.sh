bd2018aca21584d04910f27145c824e31a558658
Xandr <xandr@syneforge.com>
2015-11-13 18:33:37 +0200

    upgrade graphite

---
+++ daemons/carbon-relay.sh
@@ -0,0 +1,1 @@
+/usr/bin/python /opt/graphite/bin/carbon-relay.py start --debug 2>&1 >> /var/log/carbon-relay.log
--- scripts/local_settings.py
+++ scripts/local_settings.py
@@ -204,3 +204,5 @@
 
 LOG_DIR = '/var/log/graphite'
 SECRET_KEY = '$(date +%s | sha256sum | base64 | head -c 64)'
+import os
+os.environ['LANG'] = 'en_US.UTF-8'
